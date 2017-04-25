1a6b01335cd14256cc95d4eb09668d0e308588dd
oNguyenVanThieu <nguyen.van.thieub@framgia.com>
2017-04-25 23:57:05 +0700

    Demo Lock Screen

---
+++ iOSOrientation/AppUtility.swift
@@ -0,0 +1,28 @@
+//
+//  AppUtility.swift
+//  iOSOrientation
+//
+//  Created by Thieu Mao on 4/25/17.
+//  Copyright © 2017 Hay Nhanh. All rights reserved.
+//
+
+import UIKit
+
+struct AppUtility {
+    
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask) {
+        
+        if let delegate = UIApplication.shared.delegate as? AppDelegate {
+            delegate.orientationLock = orientation
+        }
+    }
+    
+    /// OPTIONAL Added method to adjust lock and rotate to the desired orientation
+    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
+        
+        self.lockOrientation(orientation)
+        
+        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
+    }
+    
+}
---
+++ iOSOrientation/SecondViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  iOSOrientation
+//
+//  Created by Thieu Mao on 4/25/17.
+//  Copyright © 2017 Hay Nhanh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second ViewController")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        AppUtility.lockOrientation(.portrait)
+        // Or to rotate and lock
+         AppUtility.lockOrientation(.portrait, andRotateTo: .portrait)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Don't forget to reset when view is being removed
+        AppUtility.lockOrientation(.all)
+    }
+
+    
+}
